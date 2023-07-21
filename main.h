01ec0517ffaa5d25876c497f48668d3b2c022571
aboubakr <c.aboubakr@gmail.com>
2023-07-21 21:52:23 +0100

    Create header file named main.h that contains functions prototypes

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+int _putchar(char);
+int _puts(char *);
+int _slen(char *);
+int _printf(const char *format, ...);
+int put_str_s(char *p);
+int put_int_d(int n, int);
+#endif /* MAIN_H */
