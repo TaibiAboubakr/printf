372edf78b54a569ddb76cd7c213c4000120b1f75
aboubakr <c.aboubakr@gmail.com>
2023-07-22 02:01:33 +0100

    Edit description of _puts function

--- put_char_str.c
+++ put_char_str.c
@@ -3,40 +3,42 @@
 #include "main.h"
 
 /**
-* _slen - function that calculate lenght of string
-* @s: pointer to string
-* Return: lenght of string
-*/
+ * _slen - function that calculate lenght of string
+ * @s: pointer to string
+ * Return: lenght of string
+ */
 int _slen(char *s)
 {
-int i = 0;
-while (s[i])
-i++;
-return (i);
+    int i = 0;
+    while (s[i])
+        i++;
+    return (i);
 }
 
 /**
-* _puts - writes the character c to stdout
-* @c: pointer to string
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+ * _puts - writes the character c to stdout
+ * @c: pointer to string
+ * Return: number of characters printed.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int _puts(char *c)
 {
-if (c == NULL){
-_puts("(null)");
-return (6);
-}
-return (write(1, c, _slen(c)));
+    if (c == NULL)
+    {
+        _puts("(null)");
+        return (6);
+    }
+    return (write(1, c, _slen(c)));
 }
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ * Return: 1 : On success .
+ *        -1 : is returned, and errno is set appropriately.
+ */
+
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
