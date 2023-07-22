e5d49ca14809267b0a4eeb42e80d414b4a1686a3
aboubakr <c.aboubakr@gmail.com>
2023-07-22 23:49:59 +0100

    Update function convert number : replaces the use of a fixed-size array with dynamic memory allocation using malloc

--- convert_number.c
+++ convert_number.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * convert_binary - function that convert decimal number to binary
  * @n : number to be converted
@@ -6,24 +8,36 @@
  */
 int convert_binary(unsigned int n)
 {
-	unsigned int arr[8] = {128, 64, 32, 16, 8, 4, 2, 1};
-	int check = 0, count = 0, i;
+	unsigned int n1 = n;
+	int check = 0, count = 0, i, k, j = 0;
+	unsigned int *p;
 
-	if (n == 0)
+	while (n1 / 2 > 0)
+	{
+	j++;
+	n1 = n1 / 2;
+	}
+
+	p = (int *)malloc(sizeof(unsigned int) * j);
+	if (p == NULL)
+	exit(-1);
+	i = j;
+	for (k = 0; k <= j; k++, i--)
+	p[k] = _pow(2, i);
+	if (n == 0 && count == 0)
 	{
 	__putchar('0');
 	count++;
 	return (count);
 	}
-	for (i = 0; i < 8; i++)
+	for (i = 0; i <= j; i++)
 	{
-
-		if (n / arr[i] > 0)
+		if (n / p[i] > 0)
 		{
 			check = 1;
 			__putchar('1');
 			count++;
-			n = n - arr[i];
+			n = n - p[i];
 		}
 		else
 		{
