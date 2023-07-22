b876420e9d7beca8fa1bf8383f1370b75b9ca253
aboubakr <c.aboubakr@gmail.com>
2023-07-22 01:54:45 +0100

    Edit file printf.c add functions skipflags and switching

--- main.h
+++ main.h
@@ -7,4 +7,7 @@ int _slen(char *);
 int _printf(const char *format, ...);
 int put_str_s(char *p);
 int put_int_d(int n, int);
+int switching(va_list, char);
+int skipflags(const char *f, int i);
+
 #endif /* MAIN_H */
