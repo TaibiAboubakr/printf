182ca7121a852570999a1d91e498be8008c238f3
imane <imanezenasnii@gmail.com>
2023-07-23 16:48:32 -0700

    Add a function that reverses strings

---
+++ reverse.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * put_rev - function that prints a reverse string
+ * @str: pointer to string
+ * Return: number of characters printed.
+ */
+int put_rev(char *str)
+{
+	int i = 0, j;
+	char *p;
+
+	if (str == NULL)
+	{
+		_puts("(null)");
+		return (6);
+	}
+	p = malloc(_slen(str) + 1)
+		if (p == NULL)
+			return (-1);
+	j = _slen(str) - 1;
+	for (i = 0; str[i]; j--, i++)
+		p[i] = str[j];
+	p[i + 1] = '\0';
+	i = _puts(p);
+	free(p);
+	return (i);
+}
