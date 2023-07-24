2651697f76d48f6d5798d0ba44d845d03851fd9d
imane <imanezenasnii@gmail.com>
2023-07-24 09:48:12 -0700

    remove unused varibles

--- printf.c
+++ printf.c
@@ -95,6 +95,33 @@ break;
 return (count);
 }
 /**
+ * switching2 -  switch between the placeholders.
+ * @args: argument.
+ * @plh: placeholder
+ * Return: number of characters skipped.
+ */
+int switching2(va_list args, char plh)
+{
+int count = 0;
+void *ptr;
+uintptr_t  addr;
+switch (plh)
+{
+case 'p':
+ptr = va_arg(args, void*);
+addr = (uintptr_t)ptr;
+count += print_addr(addr);
+break;
+default:
+__putchar('%');
+__putchar(plh);
+count += 2;
+break;
+}
+return (count);
+}
+
+/**
  * skipflags -  function that skip all unused flags.
  * @f: pointer to format
  * @i: index
